c1b2be1b46cbffae1870f06919757a7238925af2
Miguel Gamboa Sánchez <miguel.gamboasanchez@usp.ceu.es>
2025-02-20 19:29:58 +0100

    algunos titulos añadidos

--- public/javascript/chischas/conexion/crearLink.js
+++ public/javascript/chischas/conexion/crearLink.js
@@ -4,7 +4,7 @@ document.getElementById("crearLink").addEventListener("click", function(event) {
     fetch("/chischas/id")
         .then(response => response.url) // Obtiene la URL generada
         .then(url => {
-            document.getElementById("link").innerHTML = `Conéctate a: <a href="${url}">${url}</a>`;
+            document.getElementById("link").innerHTML = `Conéctate a: <a href="${url}" target="_blank">${url}</a>`;
         })
         .catch(error => console.error("Error al generar la partida:", error));
 });
